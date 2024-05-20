5165d1be9031535a4d407394aaa5cc47878afbf2
Lukas Blacklock <ljab1984@protonmail.com>
2024-05-20 19:08:17 +0100

    bibliography my life is pain

--- source/modules/bibliography.py
+++ source/modules/bibliography.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def credit_me():
-    with open('version.txt', 'r') as file:
+    with open('source/version.txt', 'r') as file:
         version_number = file.readline().strip()
     print(f"Version {version_number} of G2N2")
     print("Written by Lukas Blacklock. Email issues to ljb1crt@bolton.ac.uk or create an issue at https://github.com/lukasjab/G2N2/issues")
